ee598f623c1ce54c1eae12e82ad293ab66ba7461
Javier <javierruizdegaona@gmail.com>
2022-02-12 13:10:45 +0100

    Implementacion Test <testPersona>

---
+++ src/com/iescomercio/ed/bloque2/repaso/modelo/PersonaTest.java
@@ -0,0 +1,46 @@
+package com.iescomercio.ed.bloque2.repaso.modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PersonaTest {
+
+	@Test
+	void testPersonaStringStringString() {
+		Persona p1 = new Persona ("12345678A", "Pedro", "Gómez");
+		Persona p2 = new Persona ("87654321B", "Juan", "Pérez");
+		Persona p3 = new Persona ("87651234C", "Javier", "Ruiz de Gaona");
+	}
+
+	@Test
+	void testGetDni() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetDni() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetApellido1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetApellido1() {
+		fail("Not yet implemented");
+	}
+
+}
