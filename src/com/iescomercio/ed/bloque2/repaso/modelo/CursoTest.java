be480e3f7df596630d29f9d103c81c6983f5b9c8
Javier <javierruizdegaona@gmail.com>
2022-02-12 13:31:37 +0100

    Implementacion Test <eliminarAlumno>

--- src/com/iescomercio/ed/bloque2/repaso/modelo/CursoTest.java
+++ src/com/iescomercio/ed/bloque2/repaso/modelo/CursoTest.java
@@ -8,7 +8,22 @@ class CursoTest {
 
 	@Test
 	void testEliminarAlumno() {
-		fail("Not yet implemented");
+		Curso c1 = new Curso();
+		c1.aniadirAlumno(new Persona("72930462Q","Sara","Martinez"));
+		c1.aniadirAlumno(new Persona("00374926D","Pepe","Botella"));
+		
+		int numeroAlum1 = c1.numeroAlumnos();
+		try {
+			c1.eliminarAlumno("72930462Q");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		int numeroAlum2 = c1.numeroAlumnos();
+		
+		assertEquals(numeroAlum1 - 1, numeroAlum2);
+		
+		Exception e = assertThrows(Exception.class, ()->c1.eliminarAlumno("7358H"));
+		assertEquals(e.getMessage(), "El dni no tiene la longitud adecuada");
 	}
 
 	@Test
@@ -30,10 +45,4 @@ class CursoTest {
 	void testNumeroAlumnos() {
 		fail("Not yet implemented");
 	}
-
-	@Test
-	void testMostrarTodos() {
-		fail("Not yet implemented");
-	}
-
 }
