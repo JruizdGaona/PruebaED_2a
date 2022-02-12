0e40cdb2f5115db6a653b192c9e57e808cc90d86
Javier <javierruizdegaona@gmail.com>
2022-02-12 13:24:07 +0100

    Creacion Fichero CursoTest

---
+++ src/com/iescomercio/ed/bloque2/repaso/modelo/CursoTest.java
@@ -0,0 +1,39 @@
+package com.iescomercio.ed.bloque2.repaso.modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CursoTest {
+
+	@Test
+	void testEliminarAlumno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAniadirAlumno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEstaRegistrado() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCurso() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNumeroAlumnos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMostrarTodos() {
+		fail("Not yet implemented");
+	}
+
+}
